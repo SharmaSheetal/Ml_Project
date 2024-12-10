eae8794d82adff28bc678d44ce3516f1e5d864f8
SharmaSheetal <sharmasheetal9798@gmail.com>
2024-12-09 23:19:26 -0700

    elastic bean config

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData, PerdictionPipeline
+from src.pipeline.predict_pipeline import CustomData,PredictionPipeline
 from src.logger import logging
 
 # Initialize the Flask application
